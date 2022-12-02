1ca191e7b1cca8ff9405cbd1b8aa3dbc728d3b4e
Felipe Barcon <felipe.barcon@gmail.com>
2022-12-02 23:50:15 +0100

    add routes

--- interactive-rating-component-angular/src/app/app.module.ts
+++ interactive-rating-component-angular/src/app/app.module.ts
@@ -1,22 +1,27 @@
+// Modules
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppComponent } from './app.component';
 import { RatingComponent } from './rating/rating.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { ValidationComponent } from './validation/validation.component';
+import { RouterModule } from '@angular/router';
+
+// Routes
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     RatingComponent,
     HighlightDirective,
-    ValidationComponent
+    ValidationComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
